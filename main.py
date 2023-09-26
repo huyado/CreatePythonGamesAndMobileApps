f18429d150a26df31ba00fda16b9437f47747550
huyado <vanhuydo2003@gmail.com>
2023-09-26 14:58:16 +0700

    feat: ScrollView

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
+#from kivy.uix.scrollview import ScrollView
 
 
-
+#class ScrollViewExample(ScrollView):
+#    pass
 
 
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        size = dp(100)
         for i in range(0, 100):
-            b = Button(text = str(i), size_hint=(0.2, 0.2))
+            b = Button(text = str(i), size_hint=(None, None), size = (size , size))
             self.add_widget(b)
 class GridLayoutExample(GridLayout):
     pass
